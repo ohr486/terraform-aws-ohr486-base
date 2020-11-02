a7ad309b1586e77dd7cfef530ccdef3f2954ebbe
ohr486 <ohr486@gmail.com>
2020-11-02 22:13:43 +0900

    update aws credential info

--- backend.tf
+++ backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "ohr486.terraform" # SET YOUR BUCKET
-    key            = "ohr486base.tfstate"
-    region         = "ap-northeast-1"
-    dynamodb_table = "tfstate"
+    bucket                  = "ohr486.terraform"     # SET YOUR BUCKET
+    key                     = "ohr486base.tfstate"   # SET YOUR KEY
+    region                  = "ap-northeast-1"
+    dynamodb_table          = "tfstate"              # SET YOUR DDB TABLE
+    shared_credentials_file = "~/.aws/credentials"
+    profile                 = "ohr486base-terraform" # SET YOUR PROFILE
   }
 }
