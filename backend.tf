660793e4a5b26a3d31f928e23910953f3263272f
ohr486 <ohr486@gmail.com>
2023-09-03 15:43:57 +0900

    update s3 key

--- backend.tf
+++ backend.tf
@@ -7,7 +7,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket                  = "ohr486.terraform"     # SET YOUR BUCKET
-    key                     = "ohr486base.tfstate"   # SET YOUR KEY
+    key                     = "ohr486-base.tfstate"   # SET YOUR KEY
     region                  = "ap-northeast-1"
     dynamodb_table          = "tfstate"              # SET YOUR DDB TABLE
     shared_credentials_file = "~/.aws/credentials"
