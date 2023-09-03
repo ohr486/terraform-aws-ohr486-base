7116da2aa4234c6310d3204ea832a75c9f59794d
ohr486 <ohr486@gmail.com>
2023-09-03 15:53:03 +0900

    remove sg for mysql

--- sg.tf
+++ sg.tf
@@ -27,48 +27,4 @@ resource "aws_security_group" "ssh" {
   }
 }
 
-### MySQL
-
-resource "aws_security_group" "mysql" {
-  vpc_id      = aws_vpc.ohr486base.id
-  name        = "${local.resource}-mysql"
-  description = "${local.resource}-mysql"
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = [local.cidr]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name     = "${local.resource}-mysql"
-    Resource = local.resource
-  }
-}
-
-
-## DB Security Group
-
-### MySQL
-
-resource "aws_db_security_group" "mysql" {
-  name        = "${local.resource}-mysql"
-
-  ingress {
-    cidr = local.cidr
-  }
-
-  tags = {
-    Name     = "${local.resource}-mysql"
-    Resource = local.resource
-  }
-}
 
